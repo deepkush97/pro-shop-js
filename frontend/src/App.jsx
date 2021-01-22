502914ff5e57049e669109b28e8643221725697d
Deepanshu Kushwaha <kushwaha97deepanshu@gmail.com>
2021-01-22 06:42:43 +0530

    Product page with routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,21 @@
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Container } from "react-bootstrap";
+import { HomeScreen } from "./screens/HomeScreen";
+import { ProductScreen } from "./screens/ProductScreen";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 export const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Pro shop</h1>
+          <Route exact path="/" component={HomeScreen} />
+          <Route exact path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 

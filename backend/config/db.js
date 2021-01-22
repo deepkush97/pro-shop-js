f7e178168bfe1e28d5a3b424c8e852c5b46c1ee4
Deepanshu Kushwaha <kushwaha97deepanshu@gmail.com>
2021-01-22 19:51:22 +0530

    databaseConfig and mongoose models

---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+import colors from "colors";
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connection = await mongoose.connect(process.env.MONGO_URL, {
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useNewUrlParser: true,
+    });
+    console.log(
+      `MongoDB connected : ${connection.connection.host}`.cyan.underline
+    );
+  } catch (error) {
+    console.log(error.message.red.bold.underline);
+    process.exit(1);
+  }
+};
+
+export default connectDB;

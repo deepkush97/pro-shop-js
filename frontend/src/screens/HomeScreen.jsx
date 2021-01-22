2a965945be3d07c2002041aaece80c12b0ea1efc
Deepanshu Kushwaha <kushwaha97deepanshu@gmail.com>
2021-01-22 21:43:54 +0530

    Created and displayed data via store

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,26 +1,34 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Col, Row } from "react-bootstrap";
-import axios from "axios";
 import { Product } from "../components/Product";
+import { Message } from "../components/Message";
+import { Loader } from "../components/Loader";
+import { listProducts } from "../store/actions/productActions";
 export const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]);
+  const { loading, error, products } = useSelector(
+    (state) => state.productList
+  );
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,8 +1,10 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-
-const reducers = combineReducers({});
+import { productListReducers } from "./reducers/productReducers";
+const reducers = combineReducers({
+  productList: productListReducers,
+});
 const initialState = {};
 
 const middlewares = [thunk];

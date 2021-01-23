90556b5b5089fe7052bc4862dbee70b4150a01ad
Deepanshu Kushwaha <kushwaha97deepanshu@gmail.com>
2021-01-23 15:17:49 +0530

    created seperate route

--- backend/server.js
+++ backend/server.js
@@ -3,20 +3,14 @@ dotenv.config();
 import express from "express";
 import colors from "colors";
 import connectDB from "./config/db.js";
-import Product from "./models/productModel.js";
+import productRoutes from "./routes/productRoutes.js";
 connectDB();
 const app = express();
 app.get("/", (req, res) => {
   res.send("API is working");
 });
-app.get("/api/products", async (req, res) => {
-  res.json(await Product.find());
-});
-app.get("/api/products/:id", async (req, res) => {
-  const product = await Product.findOne({ _id: req.params.id });
-  res.json(product);
-});
 
+app.use("/api/products", productRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(

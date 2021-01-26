7f3870ec466c824e63a35ca0a093555b617e9f44
Deepanshu Kushwaha <kushwaha97deepanshu@gmail.com>
2021-01-26 21:31:36 +0530

    login redux code

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -6,18 +6,25 @@ import {
   productDetailsReducer,
   productListReducer,
 } from "./reducers/productReducers";
-
+import { userLoginReducer } from "./reducers/userReducers";
 const reducers = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : {};
 
-const initialState = { cart: { cartItems: cartItemsFromStorage } };
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
+};
 
 const middlewares = [thunk];
 const store = createStore(

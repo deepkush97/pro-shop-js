5fba250cca11ea8688d1db27d3f1fca3c2b1f65e
Deepanshu Kushwaha <kushwaha97deepanshu@gmail.com>
2021-01-27 23:04:53 +0530

    profile updation backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,9 +4,13 @@ import {
   authUser,
   getUserProfile,
   registerUser,
+  updateUserProfile,
 } from "../controllers/userController.js";
 import { protect } from "../middlewares/authMiddleware.js";
 router.route("/login").post(authUser);
 router.route("/").post(registerUser);
-router.route("/profile").get(protect, getUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 export default router;

b96f667a11e3e7a4ad39a97fa3e0d675e69b7a53
Deepanshu Kushwaha <kushwaha97deepanshu@gmail.com>
2021-04-09 20:51:11 +0530

    Added: Multer and routes for multer

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,39 @@
+import express from "express";
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      `${file.filename}-${Date.now()}${path.extname(file.originalname)} `
+    );
+  },
+});
+
+const checkFileType = (file, cb) => {
+  const fileTypes = /jpg|jpeg|png/;
+  const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
+  const mimeType = fileTypes.test(file.mimetype);
+  if (extName && mimeType) {
+    return cb(null, true);
+  } else {
+    return cb("Images Only");
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    checkFileType(file, cb);
+  },
+});
+
+const router = express.Router();
+router.post("/", upload.single("image"), (req, res) => {
+  return res.send(`${req.file.path}`);
+});
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
@@ -6,6 +7,7 @@ import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 import { errorHandler, notFound } from "./middlewares/errorMiddleware.js";
 connectDB();
 const app = express();
@@ -17,9 +19,14 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
+
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
+
+app.use("/uploads", express.static(path.join(path.resolve(), "/uploads")));
+
 app.use(notFound);
 
 app.use(errorHandler);

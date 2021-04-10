40280e87b43b2fca1235c2c95df75758030c3b98
Deepanshu Kushwaha <kushwaha97deepanshu@gmail.com>
2021-04-10 12:55:30 +0530

    Added: Get Order routes

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -69,3 +69,8 @@ export const getMyOrders = asyncHandler(async (req, res) => {
   const orders = await Order.find({ user: req.user._id });
   res.json(orders);
 });
+
+export const getOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({}).populate("user", "id name");
+  res.json(orders);
+});
